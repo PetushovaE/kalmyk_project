60671fa12ece51971087abd37ed1b2a3a09df4a7
Ekaterina Petushova <epetushova@gmail.com>
2017-03-07 20:27:37 -0500

    added custom.scss to app/stylesheets

--- kalmykprojet/app/controllers/users_controller.rb
+++ kalmykprojet/app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       # params.fetch(:user, {})
-      params.require(:user).permit(:name, :city, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :city, :profile_pic, :email, :password, :password_confirmation)
     end
 end
--- kalmykprojet/app/models/user.rb
+++ kalmykprojet/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
 	mount_uploader :picture, PictureUploader
 	has_many :comments
 	has_many :articles, through: :comments
+
+	has_secure_password
 end
