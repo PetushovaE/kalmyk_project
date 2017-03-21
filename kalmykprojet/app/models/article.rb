d7ac18d05ebd8e90dff2cd120d7cc5e545ef1f30
Ekaterina Petushova <epetushova@gmail.com>
2017-03-21 10:59:03 -0400

    edited comments in article show page

--- kalmykprojet/app/models/article.rb
+++ kalmykprojet/app/models/article.rb
@@ -7,26 +7,14 @@ class Article < ApplicationRecord
 	has_many :comments, :dependent => :destroy
 	has_many :users, through: :comments
 
-	accepts_nested_attributes_for :subjects
-
-  def subjects_attributes=(subject_attributes)
-  	subject_attributes.values.each do |subject|
-  		subject = Subject.find_or_create_by(subject)
-  		self.subjects << subject
-  	end
-  end
-
-	
-	# has_many :subjects, through: :article
-	# accepts_nested_attributes_for :subjects
-
-
 	# validates :title, :body, presence: true
 
-	# def subjects_attributes=(subject_attributes)
- #    	subject_attributes.values.each do |subject_attribute|
- #      		subject = Subject.find_or_create_by(subject_attribute)
- #      		self.subjects << subject
- #    	end
- #  	end
+	accepts_nested_attributes_for :subjects
+
+  	def subjects_attributes=(subject_attributes)
+  		subject_attributes.values.each do |subject|
+	  		subject = Subject.find_or_create_by(subject)
+	  		self.subjects << subject
+  		end
+  	end
 end
